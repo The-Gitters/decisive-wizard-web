e3a58986281f948dba8746ec8698d27209a4a779
Joshua Stark <jstark8@gmail.com>
2023-04-21 10:49:09 -0400

    Lab 5 Part 2

--- frontend/src/App.test.tsx
+++ frontend/src/App.test.tsx
@@ -2,8 +2,26 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+describe('The main app', () => {
+
+  it('should render Orders link', () => {
+    render(<App />);
+    expect(screen.getByText('Orders')).toBeInTheDocument();
+  });
+
+  it('should render Payment link', () => {
+    render(<App />);
+    expect(screen.getByText('Payment')).toBeInTheDocument();
+  });
+  
+  it('should render Home link', () => {
+    render(<App />);
+    expect(screen.getByRole('link', { name: "Home" })).toBeInTheDocument();
+  });
+
+  it('should render the Home headr', () => {
+    render(<App />);
+    expect(screen.getByRole('heading', { level: 2 })).toBeInTheDocument();
+  });
+
+})
